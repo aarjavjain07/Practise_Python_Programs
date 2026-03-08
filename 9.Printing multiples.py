813e4999757acd345086e0cff0ac45939eb147d4
Aarjav Jain <aarjavj745@gmail.com>
2026-03-08 11:14:47 +0530

    Initial commit

---
+++ 9.Printing multiples.py
@@ -0,0 +1,6 @@
+# This is a python program to store first five multiples of a number in a variable
+# and print them.
+
+n = int(input("Enter the number :"))
+a,b,c,d,e = n*1,n*2,n*3,n*4,n*5
+print(a,b,c,d,e)
